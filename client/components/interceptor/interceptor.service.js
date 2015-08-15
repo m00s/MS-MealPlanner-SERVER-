3941f6d8e4d351a782a66cd82edfdd64fa46d69d
Massimiliano Sartoretto <massimilianosartoretto@gmail.com>
2015-08-14 21:28:03 -0700

    (F) Move interceptor factory in components

---
+++ client/components/interceptor/interceptor.service.js
@@ -0,0 +1,31 @@
+angular
+  .module('msMealPlannerApp.auth')
+  .factory('authInterceptor', authInterceptorFactory);
+
+authInterceptorFactory.$inject = ['$q', '$cookieStore', '$location'];
+
+function authInterceptorFactory($q, $cookieStore, $location) {
+  return {
+    // Add authorization token to headers
+    request: function (config) {
+      config.headers = config.headers || {};
+      if ($cookieStore.get('token')) {
+        config.headers.Authorization = 'Bearer ' + $cookieStore.get('token');
+      }
+      return config;
+    },
+
+    // Intercept 401s and redirect to login
+    responseError: function(response) {
+      if(response.status === 401) {
+        $location.path('/login');
+        // remove any stale tokens
+        $cookieStore.remove('token');
+        return $q.reject(response);
+      }
+      else {
+        return $q.reject(response);
+      }
+    }
+  };
+}
